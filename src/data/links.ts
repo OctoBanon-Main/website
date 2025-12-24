c3c4ee5d880adcf68aa50fe3e1956216c80f33a5
Maksim Bengraf <me@octobanon.com>
2025-12-24 23:30:14 +0700

    chore(links): update link to cat8753 website

--- src/data/links.ts
+++ src/data/links.ts
@@ -17,7 +17,7 @@ export const FRIENDS = [
   { href: "https://kostyazero.com/", text: "kostya_zero" },
   { href: "https://houl.floof.gay/", text: "Houl" },
   { href: "https://honakac.github.io/", text: "HonakAC" },
-  { href: "https://patrickstar8753.github.io/", text: "cat8753" },
+  { href: "https://cat8753.ru/", text: "cat8753" },
   { href: "https://shadowcj.rf.gd/", text: "ShadowCj" },
   { href: "https://tolya1337.ru/", text: "Tolya Gosuslugi" },
   { href: "https://cat.tolya1337.ru/", text: "CatMpeg" },
