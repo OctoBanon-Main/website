1ccf0ecc1df54c1e74b7ef8f58be79e658bd76fa
Maksim Bengraf <me@octobanon.com>
2026-01-26 02:00:42 +0700

    chore(friends): remove catmpeg link

--- src/data/links.ts
+++ src/data/links.ts
@@ -15,7 +15,7 @@ export const CONTACTS = [
   { href: "mailto:me@octobanon.com", text: "E-Mail" },
   { href: "https://t.me/OctoBanon", text: "Telegram" },
   { href: "https://matrix.to/#/@octo:octobanon.com", text: "Matrix" },
-  { href: "https://discord.com/users/839947810735521792", text: "Discord" },
+  { href: "https://discord.com/users/839947810735521792", text: "Discord" }
 ];
 
 export const FRIENDS = [
@@ -24,6 +24,5 @@ export const FRIENDS = [
   { href: "https://honakac.github.io/", text: "HonakAC" },
   { href: "https://cat8753.ru/", text: "cat8753" },
   { href: "https://shadowcj.site/", text: "ShadowCj" },
-  { href: "https://tolya1337.ru/", text: "Tolya Gosuslugi" },
-  { href: "https://cat.tolya1337.ru/", text: "CatMpeg" },
+  { href: "https://tolya1337.ru/", text: "Tolya Gosuslugi" }
 ];
