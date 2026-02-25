624b2bd015a1c4225e22468ccaa4d8db7b74fdcd
Maksim Bengraf <me@octobanon.com>
2026-02-25 10:53:17 +0700

    chore(donate): add Polygon network to list

--- src/data/donate.ts
+++ src/data/donate.ts
@@ -2,7 +2,8 @@ export const DONATE = [
   { text: "8BBo8aqs4YWeYdF1nsLLZsNcN3eHvEuhtjZtJ2CtA2KF5ReFwp8kp7bbSMV5cgbWT6JaEqPb8re3KBpMFBCFV3NSCjGUAkQ", description: "Monero (XMR)", copyable: true },
   { text: "bc1pt07kzflcvshd3g0668f6tngmlld235qjgsknvdyzuv7tkrvtkpsssvek4d", description: "Bitcoin (BTC, Taproot network)", copyable: true },
   { text: "0xf1eD338AB63440B0D143c51674980BB84eA5f0a9", description: "ETH / USDT / USDC (ERC-20 network)", copyable: true },
-  { text: "TNFkT85i4CvhcN2NVkrE5NS1RyiXhkd4FV", description: "TRX / USDT (TRC-20 network)", copyable: true }
+  { text: "TNFkT85i4CvhcN2NVkrE5NS1RyiXhkd4FV", description: "TRX / USDT (TRC-20 network)", copyable: true },
+  { text: "0xf1eD338AB63440B0D143c51674980BB84eA5f0a9", description: "POL / USDT / USDC (Polygon network)", copyable: true }
 ];
 
 export const FIAT_DONATE = [
