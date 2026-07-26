95aee4a386ae0a7aee8ec75d58730309f8bbb743
Maksim Bengraf <octobanonreserve@gmail.com>
2026-07-26 20:55:11 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    useTypeScriptCli: true,
+  },
 };
 
 export default nextConfig;
