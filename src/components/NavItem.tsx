e2ddfb7e46dc30de39d6be8da7967f1e2312a60a
Maksim Bengraf <octobanon@proton.me>
2026-08-15 11:35:38 +0700

    refactor(nav): wrap NavItem in li tag for semantic HTML

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -10,25 +10,27 @@ type Props = {
 
 export default function NavItem({ icon, href, text, isActive = false }: Props) {
     return (
-        <Link
-            href={href}
-            className={`
-                flex items-center rounded-md text-sm font-bold
-                flex-col md:flex-row h-[43px] md:h-[35px]
-                w-[100px] md:w-[145px] justify-center gap-1 md:gap-2
-                transition-colors duration-200 select-none cursor-pointer
-                hover:bg-[#39393C]
-                ${isActive ? "bg-[#39393C]" : ""}
-            `}
-        >
-            <Image
-                src={icon}
-                alt={text}
-                width={18}
-                height={18}
-                className="mt-[3px] md:mt-[-1px]"
-            />
-            {text}
-        </Link>
+        <li>
+            <Link
+                href={href}
+                className={`
+                    flex items-center rounded-md text-sm font-bold
+                    flex-col md:flex-row h-[43px] md:h-[35px]
+                    w-[100px] md:w-[145px] justify-center gap-1 md:gap-2
+                    transition-colors duration-200 select-none cursor-pointer
+                    hover:bg-[#39393C]
+                    ${isActive ? "bg-[#39393C]" : ""}
+                `}
+            >
+                <Image
+                    src={icon}
+                    alt={text}
+                    width={18}
+                    height={18}
+                    className="mt-[3px] md:mt-[-1px]"
+                />
+                {text}
+            </Link>
+        </li>
     );
 }
